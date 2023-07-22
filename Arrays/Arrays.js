5d75b5cb0520250a45450d0baff34a8eb132d1d1
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-07-22 21:18:42 +0530

    Added Arrays.js

---
+++ Arrays/Arrays.js
@@ -0,0 +1,52 @@
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.data;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+}
+
+const newArray = new MyArray();
+
+newArray.push("Kaizer");
+newArray.push("Vivy");
+newArray.push("Nero");
+
+newArray.delete(1);
+
+newArray.push("Dante");
+
+newArray.pop();
+newArray.pop();
+
+console.log(newArray);
