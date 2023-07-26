8d86013e9cdce7585558e84d9d09d0ac76ccd4ef
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-07-26 23:27:32 +0530

    Added Methods.js

---
+++ Arrays/Methods.js
@@ -0,0 +1,30 @@
+const arr = ["a", "b", "c", "d"];
+console.log(arr);
+
+// Push //
+
+arr.push("e");
+console.log(arr);
+
+// Pop //
+
+arr.pop();
+console.log(arr);
+
+// Shift //
+
+arr.shift();
+console.log(arr);
+
+// Unshift //
+
+arr.unshift("x");
+console.log(arr);
+
+// Splice //
+
+arr.splice(2, 0, "y");
+console.log(arr);
+
+arr.splice(3, 1);
+console.log(arr);
