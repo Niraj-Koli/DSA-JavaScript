4a6b037b55529faa6f76aa206873665cd57dc481
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-07-28 22:22:49 +0530

    Added BO2.js

---
+++ Big O/BO2.js
@@ -0,0 +1,20 @@
+function BO2(input) {
+    let a = 5; // O(1) //
+    let b = 10; // O(1) //
+    let c = 50; // O(1) //
+
+    for (let i = 0; i < input; i++) {
+        let x = i + 1; // O(n) //
+        let y = i + 2; // O(n) //
+        let z = i + 3; // O(n) //
+    }
+
+    for (let j = 0; j < input; j++) {
+        let p = j * 2; // O(n) //
+        let q = j * 2; // O(n) //
+    }
+
+    let whoAmI = "I Don't Know"; // O(1) //
+}
+
+// Big O(4 + 5n) -> O(n) //
