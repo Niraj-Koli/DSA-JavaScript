aca4f6a3899b10f3df8367955c5a79e74c23d0ac
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-07-28 22:23:35 +0530

    Added BO5.js

---
+++ Big O/BO5.js
@@ -0,0 +1,17 @@
+const array = [
+    {
+        tweet: "Readable",
+        date: 2012,
+    },
+    {
+        tweet: "Speed",
+        date: 2013,
+    },
+    {
+        tweet: "Memory",
+        date: 2014,
+    },
+];
+
+// Compare Tweets With Respect To Date //
+// O(n^2) -> Quadratic Time //
