34881d2eaf8f42dd8ef8ea372508b0608123d94b
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-09-12 23:36:31 +0530

    Added Longest Substring Without Repeating Characters File

---
+++ Arrays/Longest Substring Without Repeating Characters.js
@@ -0,0 +1,26 @@
+// Find the longest substring without repeating characters //
+
+const str = "pwwkew";
+
+function longestSubstring(str) {
+    let i = 0;
+    let j = 0;
+
+    let len = 0;
+
+    let map = new Map();
+
+    while (j < str.length) {
+        if (map.has(str[j])) {
+            i = Math.max(map.get(str[j]) + 1, i);
+        }
+
+        map.set(str[j], j);
+        len = Math.max(len, j - i + 1);
+        j++;
+    }
+
+    return len;
+}
+
+console.log(longestSubstring(str));
